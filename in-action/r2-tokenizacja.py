2e8efb1559331f3f0f15cbe7f0a9fb67729cdc11
mwwojcik <mw.wojcik@gmail.com>
2019-02-18 00:15:18 +0100

    tworzenie wektorow

--- in-action/r2-tokenizacja.py
+++ in-action/r2-tokenizacja.py
@@ -1,4 +1,6 @@
 import numpy as np
+#Budujemy one-hot vectors - wektory stanowiące numeryczną reprezentację każdego słowa
+
 sentence=sentence = """Thomas Jefferson began building Monticello at the age of 26."""
 token_sequence = str.split(sentence)
 vocab = sorted(set(token_sequence))
