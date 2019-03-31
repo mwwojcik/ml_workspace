dc4fc606d18a1cae19ade1b0ee47114179824be7
mwwojcik <mw.wojcik@gmail.com>
2019-03-31 22:19:02 +0200

    probka uczaca obejmujaca przypadki w przeciwnym wypadku

--- generator-zbioru-regul-ml/reguly.py
+++ generator-zbioru-regul-ml/reguly.py
@@ -23,24 +23,28 @@ def utworz_reguly(aFrazyWarunkow:pd.Series):
         tagger.taguj(wynik_mnozenia['a'], 'regula_start') + ' ' + wynik_mnozenia['b'] + ' ' + tagger.taguj(
             wynik_mnozenia['c'], 'regula_stop') + ' ' + wynik_mnozenia['d'] +' .')
 
-    print("Ilość rekordów w próbce=>"+str(len(wynik)))
+    print("Ilość rekordów w próbce przed dodaniem ELSE=>" + str(len(wynik)))
 
-    return wynik
-'''
-    print(len(wynik))
+
 
     w_przeciwnym_otagowane = pd.Series(tagger.taguj(W_PRZECIWNYM_WYPADKU, 'w_przeciwnym_wypadku'))
     w_przeciwnym_x_akcje=narzedzia.wylicz_iloczyn_kartezjanski([w_przeciwnym_otagowane,AKCJE],['a','b'])
-    w_przeciwnym_x_akcje_wyplaszczone=pd.Series(w_przeciwnym_x_akcje['a']+w_przeciwnym_x_akcje['b'])
+    w_przeciwnym_x_akcje_wyplaszczone=pd.Series(w_przeciwnym_x_akcje['a']+' '+w_przeciwnym_x_akcje['b'])
+
+
 
     wynik_kopia = pd.Series(tagger.taguj(wynik_mnozenia['a'], 'regula_start') + ' ' + wynik_mnozenia['b'] + ' ' + tagger.taguj(wynik_mnozenia['c'], 'regula_stop') + ' ' + wynik_mnozenia['d'])
     reguly_x_w_przeciwnym=narzedzia.wylicz_iloczyn_kartezjanski([wynik_kopia,w_przeciwnym_x_akcje_wyplaszczone],['a','b'])
     wynik_w_przeciwny_wyplaszczony=pd.Series(reguly_x_w_przeciwnym['a']+' '+reguly_x_w_przeciwnym['b']+' .')
+    wynik_w_przeciwny_wyplaszczony_probka=wynik_w_przeciwny_wyplaszczony.sample(n=4000)
 
+
+    '''
     probka_wyplaszczona_w_przeciwnym_rand=wynik_w_przeciwny_wyplaszczony.sample(n=20000)
     wynik_rand=wynik.sample(n=90000)
-
-
-
     return wynik.sample(n=140000)#_rand.append(probka_wyplaszczona_w_przeciwnym_rand,ignore_index=True)
-'''
+    '''
+    wynik_laczony=wynik.append(wynik_w_przeciwny_wyplaszczony_probka, ignore_index=True)
+
+    print("Ilość rekordów w próbce=>" + str(len(wynik_laczony)))
+    return wynik_laczony
