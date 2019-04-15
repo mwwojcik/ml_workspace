eb0896c413a432b5db72773884b9bcb8ce9cc29c
mwwojcik <mw.wojcik@gmail.com>
2019-04-16 00:06:13 +0200

    korekta probki

--- generator-zbioru-regul-ml/warunki_logiczne.py
+++ generator-zbioru-regul-ml/warunki_logiczne.py
@@ -6,7 +6,7 @@ import narzedzia
 
 OPERATORY_ZAPRZECZENIA=pd.Series(['nie'])
 
-OPERATORY_POROWNANIA_PODSTAWOWE = pd.Series(['jest równy','jest większy lub równy', 'jest mniejszy lub równa','jest równa','jest większa lub równa', 'jest mniejsza lub równa','jest równe','jest większe lub równe', 'jest mniejsze lub równe'])
+OPERATORY_POROWNANIA_PODSTAWOWE = pd.Series(['jest równy' ,'jest równa', 'jest równe'])
 
 OPERATORY_POROWNANIA_STOPNIOWANE = pd.Series(['jest większy', 'jest mniejszy', 'jest różny','jest większa', 'jest mniejsza', 'jest różna','jest większe', 'jest mniejsze', 'jest różne'])
 OPERATORY_STOPNIOWANIA = pd.Series(['niż', 'od'])
@@ -46,7 +46,7 @@ def utworz_warunki_laczone(warunki_pojedyncze:pd.Series):
     operatory_logiczne_otagowane=pd.Series(OPERATORY_LOGICZNE.apply(lambda x:tagger.taguj(x,'operator_logiczny')))
     wynik_mnozenia=narzedzia.wylicz_iloczyn_kartezjanski([warunki_lewa_strona, operatory_logiczne_otagowane, warunki_prawa_strona],['a','b','c'])
     #splaszczam i wybieram losowo n elementow gdzie n to dlugosc wektora WE
-    return pd.Series(wynik_mnozenia['a']+' '+wynik_mnozenia['b']+' '+wynik_mnozenia['c']).sample(n=2*len(warunki_pojedyncze))
+    return pd.Series(wynik_mnozenia['a']+' '+wynik_mnozenia['b']+' '+wynik_mnozenia['c']).sample(n=4*len(warunki_pojedyncze))
 
 def utworz_warunki():
     warunki_pojedyncze_otagowane=pd.Series(tagger.taguj('xxx','porowanie_OL')+' '+wynik.apply(lambda x:tagger.taguj(x,'operator_porownania'))+' '+tagger.taguj('xxx','porowanie_OP'))
