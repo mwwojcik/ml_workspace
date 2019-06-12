0589ec868a0a9c563db70a303259debb0f8f3b70
mwwojcik <mw.wojcik@gmail.com>
2019-06-13 00:01:51 +0200

    eksperymenty z probka

--- generator-zbioru-regul-ml/reguly.py
+++ generator-zbioru-regul-ml/reguly.py
@@ -26,25 +26,19 @@ def utworz_reguly(aFrazyWarunkow:pd.Series):
     print("Ilość rekordów w próbce przed dodaniem ELSE=>" + str(len(wynik)))
 
 
-
     w_przeciwnym_otagowane = pd.Series(tagger.taguj(W_PRZECIWNYM_WYPADKU, 'SK_SAN'))
     w_przeciwnym_x_akcje=narzedzia.wylicz_iloczyn_kartezjanski([w_przeciwnym_otagowane,AKCJE],['a','b'])
     w_przeciwnym_x_akcje_wyplaszczone=pd.Series(w_przeciwnym_x_akcje['a']+' '+w_przeciwnym_x_akcje['b'])
 
 
 
-    wynik_kopia = pd.Series(tagger.taguj(wynik_mnozenia['a'], 'SK_SW') + ' ' + wynik_mnozenia['b'] + ' ' + tagger.taguj(wynik_mnozenia['c'], 'SK_SW') + ' ' + wynik_mnozenia['d'])
+    wynik_kopia = pd.Series(tagger.taguj(wynik_mnozenia['a'], 'SK_SW') + ' ' + wynik_mnozenia['b'] + ' ' + tagger.taguj(wynik_mnozenia['c'], 'SK_SW') + ' ' + wynik_mnozenia['d']).sample(10000)
     reguly_x_w_przeciwnym=narzedzia.wylicz_iloczyn_kartezjanski([wynik_kopia,w_przeciwnym_x_akcje_wyplaszczone],['a','b'])
     wynik_w_przeciwny_wyplaszczony=pd.Series(reguly_x_w_przeciwnym['a']+' '+reguly_x_w_przeciwnym['b']+' .')
-    wynik_w_przeciwny_wyplaszczony_probka=wynik_w_przeciwny_wyplaszczony.sample(n=4000)
 
 
-    '''
-    probka_wyplaszczona_w_przeciwnym_rand=wynik_w_przeciwny_wyplaszczony.sample(n=20000)
-    wynik_rand=wynik.sample(n=90000)
-    return wynik.sample(n=140000)#_rand.append(probka_wyplaszczona_w_przeciwnym_rand,ignore_index=True)
-    '''
-    wynik_laczony=wynik.append(wynik_w_przeciwny_wyplaszczony_probka, ignore_index=True)
+    wynik_laczony=wynik.append(wynik_w_przeciwny_wyplaszczony.sample(30000), ignore_index=True)
 
-    print("Ilość rekordów w próbce=>" + str(len(wynik_laczony)))
-    return wynik_laczony
+    print("Ilość rekordów w próbce=>" + str(len(wynik)))
+    #return wynik_laczony
+    return wynik
--- generator-zbioru-regul-ml/warunki_logiczne.py
+++ generator-zbioru-regul-ml/warunki_logiczne.py
@@ -7,10 +7,13 @@ import narzedzia
 OPERATORY_ZAPRZECZENIA=pd.Series(['nie'])
 
 OPERATORY_POROWNANIA_PODSTAWOWE = pd.Series(['jest równy' ,'jest równa', 'jest równe'])
-
 OPERATORY_POROWNANIA_STOPNIOWANE = pd.Series(['jest większy', 'jest mniejszy', 'jest różny','jest większa', 'jest mniejsza', 'jest różna','jest większe', 'jest mniejsze', 'jest różne'])
+
 OPERATORY_STOPNIOWANIA = pd.Series(['niż', 'od'])
 operatory_stopniowane=podaj_dwuwymiarowy_iloczyn_kartezjanski(OPERATORY_POROWNANIA_STOPNIOWANE, OPERATORY_STOPNIOWANIA)
+
+
+
 #splaszczenie
 wynik=pd.Series(operatory_stopniowane['a']+' '+operatory_stopniowane['b'])
 
@@ -41,24 +44,33 @@ def utworz_warunki_czy_null():
     return pd.Series(tagger.taguj('xxx','porowanie_czy_null_OP')+' '+OPERATORY_CZY_NULL.apply(lambda x:tagger.taguj(x,'operator_porownania_czy_null')))
 
 def utworz_warunki_laczone(warunki_pojedyncze:pd.Series):
-    warunki_lewa_strona=warunki_pojedyncze.copy()
-    warunki_prawa_strona=warunki_pojedyncze.copy()
+    warunki_lewa_strona=warunki_pojedyncze.copy()#.sample(30)
+    warunki_prawa_strona=warunki_pojedyncze.copy()#.sample(30)
     operatory_logiczne_otagowane=pd.Series(OPERATORY_LOGICZNE.apply(lambda x:tagger.taguj(x,'OPR_LOG')))
     wynik_mnozenia=narzedzia.wylicz_iloczyn_kartezjanski([warunki_lewa_strona, operatory_logiczne_otagowane, warunki_prawa_strona],['a','b','c'])
     #splaszczam i wybieram losowo n elementow gdzie n to dlugosc wektora WE
-    return pd.Series(wynik_mnozenia['a']+' '+wynik_mnozenia['b']+' '+wynik_mnozenia['c']).sample(n=4*len(warunki_pojedyncze))
+    return pd.Series(wynik_mnozenia['a']+' '+wynik_mnozenia['b']+' '+wynik_mnozenia['c'])#.sample(n=4*len(warunki_pojedyncze))
 
 def utworz_warunki():
+
     warunki_pojedyncze_otagowane=pd.Series(tagger.taguj('xxx','OP_L')+' '+wynik.apply(lambda x:tagger.taguj(x,'OPR_REL'))+' '+tagger.taguj('xxx','OP_P'))
     print("warunki_pojedyncze_otagowane(ilosc_elem)"+str(len(warunki_pojedyncze_otagowane)))
-    #return warunki_pojedyncze_otagowane
-    warunki_laczone_otagowane=utworz_warunki_laczone(warunki_pojedyncze_otagowane)
+
+    warunki_laczone_otagowane = utworz_warunki_laczone(warunki_pojedyncze_otagowane)
     print("warunki_laczone_otagowane(ilosc_elem)" + str(len(warunki_laczone_otagowane)))
+
+    warunki_razem = warunki_pojedyncze_otagowane.append(
+        warunki_laczone_otagowane)  # .append(warunki_czy_null_otagowane).append(warunki_czy_null_laczone)
+    print("warunki_razem(ilosc_elem)" + str(len(warunki_razem)))
+    return warunki_razem
+
+
+    '''
+    
     warunki_czy_null_otagowane=utworz_warunki_czy_null()
     print("warunki_czy_null_otagowane(ilosc_elem)" + str(len(warunki_czy_null_otagowane)))
     warunki_czy_null_laczone = utworz_warunki_laczone(warunki_czy_null_otagowane)
     print("warunki_czy_null_laczone(ilosc_elem)" + str(len(warunki_czy_null_laczone)))
-    warunki_razem=warunki_pojedyncze_otagowane.append(warunki_laczone_otagowane)#.append(warunki_czy_null_otagowane).append(warunki_czy_null_laczone)
-    print("warunki_razem(ilosc_elem)" + str(len(warunki_razem)))
-    return warunki_razem
+    
+    '''
 
