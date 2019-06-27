bd9458369729953eb4e1da4e1bfcca5a3ba3646c
mwwojcik <mw.wojcik@gmail.com>
2019-06-28 01:12:03 +0200

    regresja prosta

---
+++ kurs-ml-fundamenty/modul-4/regresja_zlozona.py
@@ -0,0 +1,15 @@
+import pandas as pd
+#regresja liniowa ale z analiza wielu czynnikow
+
+#**** wczytywanie modelu
+df=pd.read_csv('diamonds_single.csv',header=0,sep=';')
+print(df.head())
+#**** przygotowanie danych
+
+#**** podzial na dane treningowe i testowe
+
+#**** przygotowanie modelu
+
+#**** predykcja na danych tekstowych
+
+#**** wyniki
